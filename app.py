3d20ba54ef252df32f5d12858f60bb8c2e35b905
Belal Daqqah <belaldaqqah@rfs.edu.ps>
2017-07-24 11:47:08 +0300

    informative message

--- app.py
+++ app.py
@@ -2,15 +2,39 @@ from flask import Flask, render_template, request, redirect, url_for
 import dataset
 
 app = Flask(__name__)
-# TODO: connect your database here
+db =  dataset.connect('postgres://aotvgbebjecnxl:8592d2ec4231cbe4641ec6c452a51dc41e0beef794b2fa4c42515fc4b4e93f1a@ec2-184-73-199-72.compute-1.amazonaws.com:5432/d46l183jamtfom')
 
+@app.route('/register' , methods=["GET","POST"])
+def register():
+	UsersTable = db["users"]
+	if request.method == "GET":
+		return render_template("register.html")
+	else:  
+		first_name = request.form["first_name"]
+		last_name = request.form["last_name"]
+		email = request.form["email"]
+		username = request.form["username"]
+		hometown = request.form["hometown"]
+		personal_website = request.form['personal_website']
+		entry = {"first_name":first_name ,"last_name":last_name, "email":email, "username":username, "hometown":hometown}
+		nameTocheck = username
+		results = list(UsersTable.find(username = nameTocheck))
+		if len(results) == 0:
+			UsersTable.insert(entry)
+			return redirect ("/home")
+		return render_template('home.html', first_name=first_name , last_name=last_name , 
+			email=email, username=username, hometown=hometown, personal_website=personal_website)
 
-@app.route('/')
 @app.route('/home')
 def homepage():
 	return render_template('home.html')
 
-# TODO: route to /list
+@app.route('/list')
+def listt():
+	UsersTable = db["users"]
+	allUsers = list(UsersTable.all())
+	print allUsers
+	return render_template('list.html' , users= allUsers)
 
 # TODO: route to /feed
 
