55f6d65ea458492a0a0b1eecda47ccf35de5c70a
Mohammad Arori <mohammadsaleh@rfs.edu.ps>
2017-07-26 10:17:18 +0300

    final functionality

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for, session 
+from flask import Flask, render_template, request, redirect, url_for, session, escape 
 import dataset
 from time import localtime, strftime
 app = Flask(__name__)
 db =  dataset.connect('postgres://aotvgbebjecnxl:8592d2ec4231cbe4641ec6c452a51dc41e0beef794b2fa4c42515fc4b4e93f1a@ec2-184-73-199-72.compute-1.amazonaws.com:5432/d46l183jamtfom')
+app.secret_key = 'A0Zr98k/3yX R~XHH!jmN]LWX/,?RT'
 
 @app.route('/register' , methods=["GET","POST"])
 def register():
@@ -16,10 +17,12 @@ def register():
 		username = request.form["username"]
 		hometown = request.form["hometown"]
 		personal_website = request.form['personal_website']
-		entry = {"first_name":first_name ,"last_name":last_name, "email":email, "username":username, "hometown":hometown, "personal_website":personal_website}
+		password= request.form["password"]
+		entry = {"first_name":first_name ,"last_name":last_name, "email":email, "username":username, "hometown":hometown, "personal_website":personal_website, "password": password}
 		nameTocheck = username
 		results = list(UsersTable.find(username = nameTocheck))
 		if len(results) == 0:
+			session["username"]= username
 			taken=0
 			UsersTable.insert(entry)
 			return redirect ("/list")
@@ -27,38 +30,90 @@ def register():
 			taken=1
 			
 			return render_template('home.html', first_name=first_name , last_name=last_name , 
-			email=email, username=username, hometown=hometown, personal_website=personal_website, taken = taken)
+			email=email, username=username, hometown=hometown, personal_website=personal_website,   taken = taken, )
 
 @app.route('/home')
 def homepage():
-	return render_template('home.html')
+	
+	if "username" in session:
+		username=session["username"]
+		return render_template ("home.html", username=username)
+	else:
+		return render_template('home.html')  
+	
+	
+
 
 @app.route('/list')
 def listt():
-	UsersTable = db["users"]
-	allUsers = list(UsersTable.all())
-	print allUsers
-	return render_template('list.html' , users= allUsers)
+	
+	if "username" in session:
+		UsersTable = db["users"]
+		allUsers = list(UsersTable.all())
+		print allUsers
+		return render_template('list.html' , users= allUsers)
+	else:
+		return redirect("/error")
 
 
 @app.route('/feed', methods=["GET","POST"])
 def newsfeed():
 	feedTable=db["feed"]
+	allposts = list(feedTable.all())
 	if request.method == "GET":
-		allposts = list(feedTable.all())
 		return render_template("feed.html" ,allposts=allposts)
 	else:
+		UsersTable = db["users"]
 		username = request.form["username"]
 		post= request.form["post"]
 		time = strftime("%Y-%m-%d %H:%M:%S", localtime())
 		entry = {"post":post,"username": username, "time":time}
+		nameTocheck = username
+		results = list(UsersTable.find(username = nameTocheck))		
+
+	if len(results) == 1:
+		taken=1
 		feedTable.insert(entry)
 		allposts = list(feedTable.all())
 		return render_template('feed.html',post= post, username=username , allposts=allposts)
+	else: 
+		taken=0
+		return render_template("error.html")
+@app.route("/login", methods=["GET","POST"])
+def login():
+	
+    if request.method == "GET":
+        return render_template ("login.html")
+    else:
 
-# TODO: route to /register
+        UsersTable = db["users"]
+        form = request.form
+        username= form["username"]
+        password= form["password"]
+        nameToCheck = username
+        passwordToCheck=password
+        results = len(list(UsersTable.find(username = nameToCheck, password=passwordToCheck)))
+        if results > 0:
+        	login=1
+        	session["username"]=username
+        	return redirect ("/home")
+        else:
+            login=0
+            return render_template("register.html" , login=login,username= username,password=password)
 
-# TODO: route to /error
+@app.route("/logout")
+def logout():
+	if "username" in session:
+		session.pop("username", None)
+		return render_template("logout.html")
+	else:
+		return redirect("/home")
+
+
+@app.route("/error")# TODO: route to /register
+def error():
+	return render_template("error.html")
+
 
 if __name__ == "__main__":
     app.run(port=3000)
